178ad79f50ad897149fce3602659e209fca74412
Flávio M. Oota <f@oota.com.br>
2021-09-23 16:30:08 +0100

    Ably setup

---
+++ netlify/functions/hello-world/ably.js
@@ -0,0 +1,24 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const handler = async (event) => {
+  try {
+    const subject = event.queryStringParameters.name || 'World'
+
+    const ably = new Ably.Realtime(process.env.ABLY_API_KEY);
+    const channel = ably.channels.get('scribster-main');
+
+    // // Publish a message to the test channel
+    channel.publish('greeting', `${subject}`);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: `Hello ${subject}` }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }
